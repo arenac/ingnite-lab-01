4967df402fc2f42cec55c9cfee434b81f13a8ecc
Nilo Neregato <neregato.nilo@gmail.com>
2022-05-30 23:24:13 +0200

    adding redirect

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,26 +1,26 @@
-import { getAccessToken, useUser } from "@auth0/nextjs-auth0";
+import { getSession, useUser } from "@auth0/nextjs-auth0";
 import { GetServerSideProps } from "next";
-import Link from "../../node_modules/next/link";
 
 export default function Home() {
-  const { user } = useUser();
-  return (
-    <div>
-      <h1>Hey!</h1>
-
-      <pre>{JSON.stringify(user, null, 2)}</pre>
-
-      <Link href="/api/auth/login">Login</Link>
-    </div>
-  );
+  return null;
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
-  const token = getAccessToken(req, res);
+  const session = getSession(req, res);
 
-  console.log(token);
-
-  return {
-    props: {},
-  };
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/api/auth/login",
+        permanent: false,
+      },
+    };
+  } else {
+    return {
+      redirect: {
+        destination: "/app",
+        permanent: false,
+      },
+    };
+  }
 };
