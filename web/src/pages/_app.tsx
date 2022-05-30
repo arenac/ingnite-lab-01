8aa3075fb6fffb1dbdb856619e6b0b08b6d3689e
Nilo Neregato <neregato.nilo@gmail.com>
2022-05-30 22:42:07 +0200

    web auth0 setup

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import { UserProvider } from "@auth0/nextjs-auth0";
 
 function MyApp({ Component, pageProps }) {
+  const { user } = pageProps;
+
   return (
     <UserProvider>
       <Component {...pageProps} />
--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,3 +1,26 @@
+import { getAccessToken, useUser } from "@auth0/nextjs-auth0";
+import { GetServerSideProps } from "next";
+import Link from "../../node_modules/next/link";
+
 export default function Home() {
-  return <h1>Ignite lab</h1>;
+  const { user } = useUser();
+  return (
+    <div>
+      <h1>Hey!</h1>
+
+      <pre>{JSON.stringify(user, null, 2)}</pre>
+
+      <Link href="/api/auth/login">Login</Link>
+    </div>
+  );
 }
+
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+  const token = getAccessToken(req, res);
+
+  console.log(token);
+
+  return {
+    props: {},
+  };
+};
