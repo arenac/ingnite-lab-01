6791f32325ba465762ff4c8798a9ec4da12dd40a
Nilo Neregato <neregato.nilo@gmail.com>
2022-05-30 23:27:47 +0200

    purchase and classroom auth testing route

---
+++ purchases/src/test/test.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get, UseGuards } from '@nestjs/common';
+
+import { AuthorizationGuard } from 'src/http/auth/authorization.guard';
+
+@Controller('test')
+export class TestController {
+  @Get()
+  @UseGuards(AuthorizationGuard)
+  hello() {
+    return 'Ok';
+  }
+}
