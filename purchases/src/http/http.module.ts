02640def7a785f7890dbcbcc4c2f60c36c8a713c
Nilo Neregato <neregato.nilo@gmail.com>
2022-07-06 23:01:18 +0200

    kafka config and emit message to topic

--- purchases/src/http/http.module.ts
+++ purchases/src/http/http.module.ts
@@ -5,6 +5,7 @@ import { GraphQLModule } from '@nestjs/graphql';
 import path from 'node:path';
 
 import { DatabaseModule } from '../database/database.module';
+import { MessagingModule } from '../messaging/messaging.module';
 import { CustomersService } from '../services/customers.service';
 import { ProductsService } from '../services/products.service';
 import { PurchasesService } from '../services/purchases.service';
@@ -15,6 +16,7 @@ import { PurchaseResolver } from './graphql/resolvers/purchases.resolver';
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    MessagingModule,
     DatabaseModule,
     GraphQLModule.forRoot({
       driver: ApolloDriver,
--- purchases/src/services/purchases.service.ts
+++ purchases/src/services/purchases.service.ts
@@ -2,10 +2,11 @@ import { Injectable } from '@nestjs/common';
 
 import { PrismaService } from '../database/prisma/prisma.service';
 import { CreatePurchaseParams } from '../http/graphql/inputs/create-purchase-input';
+import { KafkaService } from '../messaging/kafka.service';
 
 @Injectable()
 export class PurchasesService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService, private kafka: KafkaService) {}
 
   async listAllPurchases() {
     return this.prisma.purchase.findMany({
@@ -37,12 +38,32 @@ export class PurchasesService {
       throw new Error('Product not found');
     }
 
-    return this.prisma.purchase.create({
+    const purchase = await this.prisma.purchase.create({
       data: {
         productId,
         customerId,
         status: 'PENDING',
       },
     });
+
+    const customer = await this.prisma.customer.findUnique({
+      where: {
+        id: customerId,
+      },
+    });
+
+    this.kafka.emit('purchases.new-purchase', {
+      customer: {
+        authUserId: customer.authUserId,
+      },
+
+      product: {
+        id: product.id,
+        title: product.title,
+        slug: product.slug,
+      },
+    });
+
+    return purchase;
   }
 }
