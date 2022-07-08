9027ef9e239ed26c0ffec8047e70c38afb9c817c
Nilo Neregato <neregato.nilo@gmail.com>
2022-07-09 00:35:26 +0200

    student as user object type

--- classroom/src/http/graphql/resolvers/students.resolver.ts
+++ classroom/src/http/graphql/resolvers/students.resolver.ts
@@ -1,10 +1,15 @@
 import { UseGuards } from '@nestjs/common';
-import { Parent, Query, ResolveField, Resolver } from '@nestjs/graphql';
+import {
+  Parent,
+  Query,
+  ResolveField,
+  Resolver,
+  ResolveReference,
+} from '@nestjs/graphql';
 import { EnrollmentsService } from '../../../services/enrollments.service';
 
 import { StudentsService } from '../../../services/students.service';
 import { AuthorizationGuard } from '../../auth/authorization.guard';
-import { AuthUser, CurrentUser } from '../../auth/current-user';
 import { Enrollment } from '../models/enrollment';
 
 import { Student } from '../models/student';
@@ -16,11 +21,11 @@ export class StudentsResolver {
     private enrollmentsService: EnrollmentsService,
   ) {}
 
-  @Query(() => Student)
-  @UseGuards(AuthorizationGuard)
-  me(@CurrentUser() user: AuthUser) {
-    return this.studentsService.getStudentByAuthUserId(user.sub);
-  }
+  // @Query(() => Student)
+  // @UseGuards(AuthorizationGuard)
+  // me(@CurrentUser() user: AuthUser) {
+  //   return this.studentsService.getStudentByAuthUserId(user.sub);
+  // }
 
   @Query(() => [Student])
   @UseGuards(AuthorizationGuard)
@@ -31,5 +36,10 @@ export class StudentsResolver {
   @ResolveField(() => [Enrollment])
   enrollments(@Parent() student: Student) {
     return this.enrollmentsService.listEnrollmentsByStudentId(student.id);
+  }
+
+  @ResolveReference()
+  resolveReference(reference: { authUserId: string }) {
+    return this.studentsService.getStudentByAuthUserId(reference.authUserId);
   }
 }
